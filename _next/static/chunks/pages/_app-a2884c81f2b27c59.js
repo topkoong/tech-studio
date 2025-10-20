ebd454a938d04b877abfea24941bd93e6b4905a4
toppy <theerut@shoplive.dev>
2025-10-20 22:33:22 +0700

    Deploy static site to GitHub Pages

---
+++ _next/static/chunks/pages/_app-a2884c81f2b27c59.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{8042:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(1229)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(8042),n(1345))),_N_E=_.O()}]);
